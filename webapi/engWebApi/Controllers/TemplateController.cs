02f25275664416a38458aab46234e5e3c0bf41b2
frusciante88 <alessandro.relati@gmail.com>
2015-06-01 16:11:07 +0200

    TemplateController

---
+++ webapi/engWebApi/Controllers/TemplateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace engWebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class TemplateController : ApiController
+    {
+        [HttpGet]
+        public object Get()
+        {
+            return "Questa è una prova";
+        }
+    }
+}
