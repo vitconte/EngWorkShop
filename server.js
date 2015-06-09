2b5a5c45eb77650260f3f16134aba714a41925da
frusciante88 <alessandro.relati@gmail.com>
2015-06-09 17:23:16 +0200

    add auth controller

--- server.js
+++ server.js
@@ -4,6 +4,9 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+
+var secret = 'EngWorkShop';
 
 var app = express();
 
@@ -11,6 +14,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname));
+
+// Funzioni per la creazione del TOKEN
+
+var createToken = function(username) {
+    return jwt.sign({ username: username }, secret);
+};
+
+var decodeToken = function(token){
+    return jwt.verify(token, secret);
+};
+
+app.post('/api/Authentication', function(req, res, next){
+
+    console.log(req);
+
+    // Controlli validazione utente se OK
+
+    var result =  {
+        username: req.body.username,
+        token: createToken({username: req.body.username}),
+        loginRedirect: null
+    };
+
+    res.json(result);
+    next();
+
+});
 
 var server = app.listen(3000, function () {
 
